9724cd7df5654cb24b93559a7aa531ba5b2eeca7
Shrivatsa <shrivatsaddesai@gmail.com>
2026-06-29 17:38:07 +0530

    Add ci/cd  Pipelines 3

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket       = "terraform-state-54"
-    key          = "training-terraform/terraform-2.0.state"
+    key          = "training-terraform/terraform-3.0.state"
     region       = "ap-south-1"
     use_lockfile = true
   }
